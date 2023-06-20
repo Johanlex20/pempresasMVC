a514d64b22133d4f2a00e21e78c083b1a7f0312d
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-06-19 23:21:38 -0500

    prueba
    prueba

--- controllers/aplicarController.php
+++ controllers/aplicarController.php
@@ -45,7 +45,7 @@ class AplicarController{
             // debuguear($aprendizId); BUSQUEDA DEL ID DEL APRENDIZ
 
             // debuguear($aplicar);
-            // debuguear($errores);
+            // debuguear($errores)fdfdfdfd;
 
             if(empty($errores)){
                 $aplicar->guardar();
